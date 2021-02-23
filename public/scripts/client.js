c2cf75e37a10c65c7887be0e91a46e02d82ecee7
Samantha Knoop <sknoop@hotmail.ca>
2021-02-23 23:04:03 +0000

    create counter for number of characters left to write in a tweet. This code is in a separate file from html code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ // pair this with monitorEvents(document.getElementById("tweet-text")) written in browser's console to view details
+$(() => {
+  document.addEventListener("dblclick", (event) => {
+    console.log(event);
+  });
+})
