696d99e863251ab3475be983a000fbb85c3830d1
Samantha Knoop <sknoop@hotmail.ca>
2021-02-24 00:19:51 +0000

    dry up code by separating document.ready function from character counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,4 @@
-$(document).ready(function() {
-  
-  //counts all items entered into form, including the number of backspace key presses needed to delete everything in form
+const characterCounter = function (){
   $("#tweet-text").on("input", function() {
     const max = 140;
     let length = $(this).val().length;
@@ -12,34 +10,10 @@ $(document).ready(function() {
       $("#counter").css("color", "#545149")
       $("#counter").text(`${numOfCharsLeft} characters left`)
     }
-    console.log(this);
+    console.log("whatttttt!", this);
   });
+}
 
-  
-  
+$(document).ready(function () {
+  characterCounter();
 });
-
-// notifies you when you change where on page you're focused
-  // $("#tweet-text").on("change", function() {
-  //   console.log("change");
-  // });
-
-  //when you leave the input field
-  // $("#tweet-text").on("blur", function() {
-  //   console.log("blur");
-  // });
-
-  //will count as long as you have a key pressed
-  // $("#tweet-text").on("keydown", function() {
-  //   console.log("keydown");
-  // });
-
-  //will count as long as you have a key pressed
-  // $("#tweet-text").on("keyup", function() {
-  //   console.log("keyup");
-  // });
-
-  // will count the number of characters typed in, but not deleted from form
-  // $("#tweet-text").on("keypress", function() {
-  //   console.log("keypress");
-  // });
